ff9f2e29d1c9f855733f7c48cf296a68876c7059
UnderScore <drojasg25@gmail.com>
2024-08-04 13:24:16 -0400

    Add Ray Colors

--- main.cpp
+++ main.cpp
@@ -1,17 +1,47 @@
 #include <iostream>
 
 #include <vec3.h>
+#include <ray.h>
 #include <color.h>
 
 //File outputs a basic ppm file
 
+//Gets the rays color
+color ray_color(const ray& r) {
+    vec3 unit_dir = unit_vector(r.direction());
+    auto a = 0.5*(unit_dir.y() + 1.0);
+    return (1.0-a)*color(1.0,1.0,1.0) + a*color(0.5,0.7,1.0);
+}
+
 int main() {
+    //Camera Params
+    auto aspect_ratio = 16.0 / 9.0;
+    auto view_height = 2.0;
+    auto focal_len = 1.0;
+    auto cam_center = point3(0,0,0);
+
+
     //Image Parameters
 
-    int img_width = 1000;
-    int img_height = 1000;
+    int img_width = 2000;
+    int img_height = int(img_width / aspect_ratio);
+    
+    img_height = (img_height < 1) ? 1 : img_height;
 
-    const float FULLBRIGHTNESS = 255.999;
+    //viewport width based off the img
+    auto view_width = view_height * (double(img_width)/img_height);
+    
+    //the horizontal and vertical viewport edges as vectors
+    auto view_u = vec3(view_width, 0, 0);
+    auto view_v = vec3(0, -view_height, 0);
+
+    //Change in pixel over the viewport
+    auto pixel_delta_u = view_u / img_width;
+    auto pixel_delta_v = view_v / img_height;
+
+    //calculating the top-left pixel
+    auto view_up_left = cam_center - vec3(0,0,focal_len) - view_u/2 - view_v/2;
+    auto pixel00_loc = view_up_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     //Rendering & Outputing the image
 
@@ -20,8 +50,13 @@ int main() {
     for (int j = 0; j < img_height; j++){
         std::clog << "\rScanlines remaining: " << (img_height - j) << ' ' << std::flush;
         for (int i = 0; i < img_width; i++){
-            auto pixel_color = color(double(i)/(img_width-1), double(j)/(img_height-1), 0);
-            write_color(std::cout, pixel_color);
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_dir = pixel_center - cam_center;
+
+            ray r(cam_center, ray_dir);
+
+            color color_pixel = ray_color(r);
+            write_color(std::cout, color_pixel);
         }
     }
 
